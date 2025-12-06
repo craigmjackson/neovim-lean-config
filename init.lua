625971561d409f1303ea13a90d6c3c972cac01e4
Craig Jackson <craigmjackson@gmail.com>
2025-12-05 19:32:40 -0800

    Added options, keymaps, autocmds

--- init.lua
+++ init.lua
@@ -3,6 +3,55 @@ local misc = require('lua/misc')
 
 local installed = {}
 
+--- Options
+-- Enable line numbers
+vim.opt.number = true
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+-- Enable mouse support
+vim.opt.mouse = 'nvi'
+-- Sync with the OS clipboard
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+-- Make undo files
+vim.opt.undofile = true
+-- Make searches case-insensitive in general
+vim.opt.ignorecase = true
+-- If there is a mixed case in your search, make it case-sensitive
+vim.opt.smartcase = true
+-- Only show sign column if needed
+vim.opt.signcolumn = 'yes'
+-- Write swapfile to disk if no activity for 250ms
+vim.opt.updatetime = 250
+-- How long for other sequences to timeout
+vim.opt.timeoutlen = 300
+-- Horizontal splits on the right
+vim.opt.splitright = true
+-- Vertical splits below
+vim.opt.splitbelow = true
+-- Highlight the line with the cursor
+vim.opt.cursorline = true
+-- Keep some lines above and below the cursor
+vim.opt.scrolloff = 5
+-- Enable 256 colors even in text mode
+vim.opt.termguicolors = true
+-- Transparency for floating windows
+vim.opt.winblend = 30
+
+--- Keymaps
+-- Clear search highlighting with <Esc>
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+--- Autocommands
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
+
 --- Theme
 vim.schedule(function()
   packager.install_package(installed, 'folke/tokyonight.nvim', 'tokyonight')
